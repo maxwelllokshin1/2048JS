1cc5aeed61a4ccf9966a2562154a076ee5d24fd7
Maxwell Lokshin <139578323+maxwelllokshin1@users.noreply.github.com>
2025-01-09 22:03:49 -0500

    Add files via upload

--- game.js
+++ game.js
@@ -25,11 +25,45 @@ function draw()
     ctx.font = 'bold 20px sans-serif';
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
-    arrayBlocks.forEach(function(element){
-        ctx.fillStyle = 'yellow';
+    arrayBlocks.forEach(function(element) {
+        switch (element.value) {
+            case 2:
+                ctx.fillStyle = '#eee4da';
+                break;
+            case 4:
+                ctx.fillStyle = '#ede0c8';
+                break;
+            case 8:
+                ctx.fillStyle = '#f2b179';
+                break;
+            case 16:
+                ctx.fillStyle = '#f59563';
+                break;
+            case 32:
+                ctx.fillStyle = '#f67c5f';
+                break;
+            case 64:
+                ctx.fillStyle = '#f65e3b';
+                break;
+            case 128:
+                ctx.fillStyle = '#edcf72';
+                break;
+            case 256:
+                ctx.fillStyle = '#edcc61';
+                break;
+            case 512:
+                ctx.fillStyle = '#edc850';
+                break;
+            case 1024:
+                ctx.fillStyle = '#edc53f';
+                break;
+            case 2048:
+                ctx.fillStyle = '#edc22e';
+                break;
+        }
         ctx.fillRect(element.x, element.y, element.width, element.height);
         ctx.fillStyle = 'black';
-        ctx.fillText(element.value, element.x+element.width/2, element.y+element.height / 2);
+        ctx.fillText(element.value, element.x + element.width / 2, element.y + element.height / 2);
     });
 }
 
@@ -42,43 +76,134 @@ function addBlock()
         y: Math.floor(Math.random() * 4) * block.height,
         width: canvas.width/4,
         height: canvas.height/4,    
-        value: Math.floor(Math.random()*2) ? 2 : 4
+        value: Math.random() < 0.9 ? 2 : 4
     }
 
-    for(let element of arrayBlocks)
-    {
-        while(newBlock.x === element.x && newBlock.y === element.y)
-        {
-            newBlock.x =  Math.floor(Math.random() * 4) * block.width;
-            newBlock.y =  Math.floor(Math.random() * 4) * block.height;
-        }
+    while (arrayBlocks.some(element => element.x === newBlock.x && element.y === newBlock.y)) {
+        newBlock.x = Math.floor(Math.random() * 4) * block.width;
+        newBlock.y = Math.floor(Math.random() * 4) * block.height;
     }
 
     arrayBlocks.push(newBlock);
 }
 
-function checkCollsion(currentBlock)
-{
-    for(let element of arrayBlocks)
-    {
-        if(currentBlock === element) continue;
-        if( currentBlock !== element && 
-            currentBlock.x < element.x+element.width && 
-            currentBlock.x+currentBlock.width > element.x &&
-            currentBlock.y < element.y+element.height && 
-            currentBlock.y+currentBlock.height > element.y && 
-            currentBlock.value !== element.value){
-            console.log('touch');
-            return true;
+function moveBlock(direction){
+    
+    let move = false;
+    let merged = [];
+
+    arrayBlocks.forEach(function(element) {
+        let originalX = element.x;
+        let originalY = element.y;
+
+        switch (direction) {
+            case 'w':  // Move up
+                while (element.y > 0 && !checkCollision(element, 'up', merged)) {
+                    element.y -= block.dy;
+                    move = true;
+                }
+                break;
+            case 'a':  // Move left
+                while (element.x > 0 && !checkCollision(element, 'left', merged)) {
+                    element.x -= block.dx;
+                    move = true;
+                }
+                break;
+            case 's':  // Move down
+                while (element.y < canvas.height - block.height && !checkCollision(element, 'down', merged)) {
+                    element.y += block.dy;
+                    move = true;
+                }
+                break;
+            case 'd':  // Move right
+                while (element.x < canvas.width - block.width && !checkCollision(element, 'right', merged)) {
+                    element.x += block.dx;
+                    move = true;
+                }
+                break;
         }
 
-        if( currentBlock !== element &&
-            currentBlock.x === element.x &&
-            currentBlock.y === element.y &&
-            currentBlock.value === element.value){
-                element.value += currentBlock.value;
-                arrayBlocks.splice(currentBlock, 1);
-                console.log('currentBlock.value' + element.value);
+        // If the block moved, add a new block to the board
+    });
+    if(move)
+    {
+        addBlock();
+    }
+    // addBlock();
+}
+
+// function checkCollision(currentBlock)
+// {
+//     for(let element of arrayBlocks)
+//     {
+//         if(currentBlock === element) continue;
+//         if( currentBlock !== element && 
+//             currentBlock.x < element.x+element.width && 
+//             currentBlock.x+currentBlock.width > element.x &&
+//             currentBlock.y < element.y+element.height && 
+//             currentBlock.y+currentBlock.height > element.y && 
+//             currentBlock.value !== element.value){
+//             console.log('touch');
+//             return true;
+//         }
+
+//         if( currentBlock !== element &&
+//             currentBlock.x === element.x &&
+//             currentBlock.y === element.y &&
+//             currentBlock.value === element.value){
+//                 element.value += currentBlock.value;
+//                 console.log('currentBlock.value' + element.value);
+//         }
+//     }
+//     return false;
+// }
+
+function checkCollision(currentBlock, direction, merged) {
+    for (let element of arrayBlocks) {
+        if (currentBlock === element) continue;
+
+        // Check for possible merging or collisions in each direction
+        switch (direction) {
+            case 'up':
+                if (currentBlock.x === element.x && currentBlock.y - block.dy === element.y) {
+                    if(currentBlock.value === element.value){
+                        element.value += currentBlock.value;
+                        merged.push(element);
+                        arrayBlocks = arrayBlocks.filter(b => b !== currentBlock);
+                        return false;
+                    }else if (currentBlock.value !== element.value){ return true; }
+                }
+                break;
+            case 'down':
+                if (currentBlock.x === element.x && currentBlock.y + block.dy === element.y) {
+                    if(currentBlock.value === element.value){
+                        element.value += currentBlock.value;
+                        merged.push(element);
+                        arrayBlocks = arrayBlocks.filter(b => b !== currentBlock);
+                        return false;
+                    }else if (currentBlock.value !== element.value){ return true; }
+                }
+                break;
+            case 'left':
+                if (currentBlock.y === element.y && currentBlock.x - block.dx === element.x) {
+                    if(currentBlock.value === element.value){
+                        element.value += currentBlock.value;
+                        merged.push(element);
+                        arrayBlocks = arrayBlocks.filter(b => b !== currentBlock);
+                        return false;
+                    }else if (currentBlock.value !== element.value){ return true; }
+                }
+                break;
+            case 'right':
+                if (currentBlock.y === element.y && currentBlock.x + block.dx === element.x) {
+                    if(currentBlock.value === element.value){
+                        element.value += currentBlock.value;
+                        merged.push(element);
+                        arrayBlocks = arrayBlocks.filter(b => b !== currentBlock);
+                        return false;
+                    }else if (currentBlock.value !== element.value){ return true; }
+                }
+                break;
         }
     }
     return false;
@@ -93,42 +218,42 @@ function checkCollsion(currentBlock)
 document.addEventListener('keydown', (event) => {
     if (['w', 'a', 's', 'd'].includes(event.key)) {
         event.preventDefault(); 
+        moveBlock(event.key);
+        // arrayBlocks.forEach(function (element) {
+        //     let originalX = element.x;
+        //     let originalY = element.y;
+        //     switch (event.key) {
+        //         case 'w':
 
-        arrayBlocks.forEach(function (element) {
-            let originalX = element.x;
-            let originalY = element.y;
-            switch (event.key) {
-                case 'w':
-
-                    if (element.y > 0 && !checkCollsion(element)) {
-                        element.y -= block.dy;
-                    }
+        //             if (element.y > 0 && !checkCollsion(element)) {
+        //                 element.y -= block.dy;
+        //             }
                     
-                    break;
-                case 'a':
-                    if (element.x > 0 && !checkCollsion(element)) {
-                        element.x -= block.dx;
-                    }
-                    break;
-                case 's':
-                    if (element.y < canvas.height - block.height && !checkCollsion(element)) {
+        //             break;
+        //         case 'a':
+        //             if (element.x > 0 && !checkCollsion(element)) {
+        //                 element.x -= block.dx;
+        //             }
+        //             break;
+        //         case 's':
+        //             if (element.y < canvas.height - block.height && !checkCollsion(element)) {
                 
-                        element.y += block.dy;
-                    }
-                    break;
-                case 'd':
-                    if (element.x < canvas.width - block.width && !checkCollsion(element)) {
-                        element.x += block.dx;
-                    }
-                    break;
-            }
+        //                 element.y += block.dy;
+        //             }
+        //             break;
+        //         case 'd':
+        //             if (element.x < canvas.width - block.width && !checkCollsion(element)) {
+        //                 element.x += block.dx;
+        //             }
+        //             break;
+        //     }
 
-            if (checkCollsion(element)) {
-                element.x = originalX;
-                element.y = originalY;
-            }
-        });
-        addBlock();
+        //     if (checkCollsion(element)) {
+        //         element.x = originalX;
+        //         element.y = originalY;
+        //     }
+        // });
+        // addBlock();
         console.log('arrayBlocks.length' + arrayBlocks.length);
     }
 });
@@ -142,7 +267,7 @@ function startGame()
     {
         gameLoop = requestAnimationFrame(loop);
 
-        if(++count < 20)
+        if(++count < 1)
         {
             return;
         }
